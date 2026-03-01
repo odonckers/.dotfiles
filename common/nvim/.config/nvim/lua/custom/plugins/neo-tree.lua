871e6a1cc78341d7bac947ffacb36161e805f037
Owen Donckers <odonckers@jw.org>
2026-03-01 14:13:59 -0500

    updated bindings for neo tree

--- common/nvim/.config/nvim/lua/custom/plugins/neo-tree.lua
+++ common/nvim/.config/nvim/lua/custom/plugins/neo-tree.lua
@@ -11,8 +11,24 @@ return {
             winbar = true,
             statusline = false,
         },
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                hide_dotfiles = false,
+            },
+            follow_current_file = {
+                enabled = true,
+                leave_dirs_open = true,
+            },
+        },
+        window = {
+            width = 56,
+        },
     },
     keys = {
-        { '<leader>E', '<cmd>Neotree<cr>', desc = 'Explore files' },
+        { '<leader>E', '<cmd>Neotree toggle<cr>', desc = 'Toggle explorer' },
+        { '<leader>F', '<cmd>Neotree filesystem<cr>', desc = 'Explore files' },
+        { '<leader>B', '<cmd>Neotree buffers<cr>', desc = 'Explore buffers' },
+        { '<leader>G', '<cmd>Neotree git_status<cr>', desc = 'Explore git' },
     },
 }

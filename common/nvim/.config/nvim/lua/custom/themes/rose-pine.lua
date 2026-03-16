a4c6ab214495811a73420789c98926759f1368d0
Owen Donckers <odonckers@jw.org>
2026-03-16 13:29:17 -0400

    light theme rose pine dark theme vague

--- common/nvim/.config/nvim/lua/custom/themes/rose-pine.lua
+++ common/nvim/.config/nvim/lua/custom/themes/rose-pine.lua
@@ -1,6 +1,7 @@
 return {
     'rose-pine/neovim',
     name = 'rose-pine',
+    enabled = false,
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other plugins
     config = function() vim.cmd('colorscheme rose-pine') end,

1815fd71851ba1e15b93d4becfbb7c769d6f182b
Owen Donckers <odonckers@gmail.com>
2026-03-31 11:30:07 -0400

    globally set style

--- common/nvim/.config/nvim/init.lua
+++ common/nvim/.config/nvim/init.lua
@@ -96,33 +96,9 @@ vim.pack.add({
 
 -- Pack: Vague theme
 require('vague').setup({
-    style = {
-        -- "none" is the same thing as default. But "italic" and "bold" are also valid options
-        boolean = 'bold',
-        number = 'none',
-        float = 'none',
-        error = 'bold',
-        comments = 'italic',
-        conditionals = 'none',
-        functions = 'none',
-        headings = 'bold',
-        operators = 'none',
-        strings = 'none',
-        variables = 'none',
-
-        -- keywords
-        keywords = 'none',
-        keyword_return = 'italic',
-        keywords_loop = 'none',
-        keywords_label = 'none',
-        keywords_exception = 'bold',
-
-        -- builtin
-        builtin_constants = 'bold',
-        builtin_functions = 'none',
-        builtin_types = 'bold',
-        builtin_variables = 'none',
-    },
+    transparent = false,
+    bold = true,
+    italic = true,
     plugins = {
         lsp = {
             diagnostic_error = 'bold',

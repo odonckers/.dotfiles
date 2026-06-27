188c5650375b6f67e2f6b210e5804dc55fd9b6f9
Owen Donckers <odonckers@gmail.com>
2026-06-27 14:21:54 -0400

    wrap markdown

--- common/nvim/.config/nvim/after/ftplugin/markdown.lua
+++ common/nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,4 @@
 vim.opt_local.spell = true
 vim.opt_local.colorcolumn = '120'
+vim.opt_local.wrap = true
 -- vim.opt_local.conceallevel = 2 -- Completely hide concealed text unless it has a custom replacement character defined
